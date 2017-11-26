2b7b755290886f595a5b234b42dfde712d78da99
TTOExtreme <camarottol@hotmail.com>
2017-11-26 20:45:42 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 $(window).scroll(function() {
     if ($(this).scrollTop()>=0 ) {//not in the end of page
       $(".FadeInOut").each(function(){ ElementFadeIn(this); ElementFadeOut(this); });
+      $(".Fadein").each(function(){ ElementFadeIn(this);});
+      $(".FadeOut").each(function(){ElementFadeOut(this); });
       $(".NavBar").each(function(){MinimaliseNavbar(this,50,150);});
       $(".Nav_item").each(function(){MinimaliseNavbar(this,50,150);});
     }
 });
 $( document ).ready(function() {
   $(".FadeInOut").each(function(){ ElementFadeIn(this); ElementFadeOut(this); });
+  $(".Fadein").each(function(){ ElementFadeIn(this);});
+  $(".FadeOut").each(function(){ElementFadeOut(this); });
   $(".NavBar").each(function(){MinimaliseNavbar(this,50,150);});
   $(".Nav_item").each(function(){MinimaliseNavbar(this,50,150);});
 
@@ -32,24 +36,28 @@ function ElementFadeOut(elem){//from top to bottom
 }
 
 function MinimaliseNavbar(elem,min,max){
+
+  $(elem).css("height",NavSize(min,max))
+}
+
+function NavSize(min,max){
   var siz=1-$(window).scrollTop() / (max*3);
   if(siz>1){siz=1;}
   if(siz<0){siz=0;}
-  siz=(siz * (max - min)) + min;
-  $(elem).css("height",siz)
+  siz=(siz * (max - min)) + min;7
+  return siz;
 }
 
 $(document).ready(function(){
   $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      var target = this.hash;
-      var $target = $(target);
-
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 900, 'swing', function () {
-          window.location.hash = target;
-      });
+    var target = this.hash;
+    var $target = $(target);
+    if(target=="#top"){
+      $('html, body').stop().animate({'scrollTop': 0}, 900, 'swing', function () {});
+    }else{
+      $('html, body').stop().animate({'scrollTop': $target.offset().top - 80}, 900, 'swing', function () {});
+    }
   });
 });
